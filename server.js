e8c257aff4a9718bcbe2d9f23b27aae2d8ef6552
turkuaztaki61 <cakirhasan86@gmail.com>
2026-05-22 18:31:31 +0300

    webhook fix

--- server.js
+++ server.js
@@ -8,13 +8,26 @@ app.get("/", (req, res) => {
   res.send("WhatsApp AI Bot Çalışıyor");
 });
 
-app.post("/webhook", (req, res) => {
+app.get("/webhook", (req, res) => {
+  const verify_token = "hasan123";
 
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode && token === verify_token) {
+    console.log("WEBHOOK DOĞRULANDI");
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+app.post("/webhook", (req, res) => {
   console.log("MESAJ GELDİ:");
   console.log(req.body);
 
   res.sendStatus(200);
-
 });
 
 const PORT = process.env.PORT || 3000;
